ba3746d1b14751631802e533d1cf581a1125cf9b
seo <eskai84@gmail.com>
2022-11-23 17:02:52 +0900

    [취약점] 적절하지 않은 난수 값 사용 - PBKDF2 로 난수 생성하도록 변경
    일회용 토큰 생성 함수에서 취약점 발견

--- divas/_conf/_lib.php
+++ divas/_conf/_lib.php
@@ -211,14 +211,11 @@ function get_client_ip() {
     return $ipaddress;
 }
 
-/* 일회용 토큰 생성 uuidgen4 -> base64 인코딩 */
-function getToken() {
-	$uuid = sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
-		  mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff),
-		  mt_rand(0, 0x0fff) | 0x4000, mt_rand(0, 0x3fff) | 0x8000,
-		  mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
-		);
-	return base64_encode($uuid);
+/* 일회용 토큰 생성 */
+function getToken(){
+	require_once "_enc_lib.php";
+    return create_hash(date("smYiHd"));
 }
+
 
 ?>
